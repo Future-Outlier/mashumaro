a8d40f28e1d2d3f34224c3f26c9a02f128ebb54f
Alexander Tikhonov <fatal1ty.rnd@gmail.com>
2026-07-23 20:00:52 +0300

    Move resolve_type_alias_type to helpers

--- mashumaro/core/meta/types/pack.py
+++ mashumaro/core/meta/types/pack.py
@@ -47,6 +47,7 @@ from mashumaro.core.meta.helpers import (
     is_union,
     is_unpack,
     not_none_type_arg,
+    resolve_type_alias_type,
     resolve_type_params,
     substitute_type_params,
     type_name,
@@ -292,12 +293,6 @@ def pack_any(spec: ValueSpec) -> Expression | None:
         return spec.expression
 
 
-def _resolve_type_alias_type(t: type) -> type:
-    while is_type_alias_type(t):
-        t = t.__value__  # type: ignore[attr-defined]
-    return t
-
-
 def pack_union(
     spec: ValueSpec, args: tuple[type, ...], prefix: str = "union"
 ) -> Expression:
@@ -336,7 +331,7 @@ def pack_union(
         )
         if packer not in packers:
             if packer == "value" and not issubclass(
-                get_type_origin(_resolve_type_alias_type(type_arg)), Collection
+                get_type_origin(resolve_type_alias_type(type_arg)), Collection
             ):
                 packers.insert(0, packer)
             else:
@@ -350,7 +345,7 @@ def pack_union(
         for packer in packers:
             packer_arg_type_names = []
             for packer_arg_type in packer_arg_types[packer]:
-                packer_arg_type = _resolve_type_alias_type(packer_arg_type)
+                packer_arg_type = resolve_type_alias_type(packer_arg_type)
                 if is_generic(packer_arg_type):
                     packer_arg_type = get_type_origin(packer_arg_type)
                 packer_arg_type_name = clean_id(type_name(packer_arg_type))
@@ -366,7 +361,7 @@ def pack_union(
             else:
                 packer_arg_type_check = f"is {packer_arg_type_names[0]}"
             if packer == "value" and not issubclass(
-                _resolve_type_alias_type(packer_arg_type), Collection
+                resolve_type_alias_type(packer_arg_type), Collection
             ):
                 with lines.indent(
                     f"if value.__class__ {packer_arg_type_check}:"
